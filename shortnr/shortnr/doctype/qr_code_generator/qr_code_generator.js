38f70b8609d1d199ea3677e083578d51317a9082
Hrushikesh Deodhar <hrushikesh.deodhar@dhwaniris.com>
2025-11-24 06:15:32 +0000

    style(qr): improve formatting and readability of Copy Short URL button functionality in QR Code Generator

--- shortnr/shortnr/doctype/qr_code_generator/qr_code_generator.js
+++ shortnr/shortnr/doctype/qr_code_generator/qr_code_generator.js
@@ -159,15 +159,21 @@ frappe.ui.form.on("QR Code Generator", {
 
 		// Add Copy Short URL button
 		if (frm.doc.short_url && !frm.doc.__islocal) {
-			frm.add_custom_button(__('Copy Short URL'), () => {
-				navigator.clipboard.writeText(frm.doc.short_url).then(() => {
-					frappe.show_alert({
-						message: __('Short URL copied to clipboard!'),
-						indicator: 'green'
-					}, 3);
-				}).catch(() => {
-					frappe.msgprint(__('Failed to copy URL'));
-				});
+			frm.add_custom_button(__("Copy Short URL"), () => {
+				navigator.clipboard
+					.writeText(frm.doc.short_url)
+					.then(() => {
+						frappe.show_alert(
+							{
+								message: __("Short URL copied to clipboard!"),
+								indicator: "green",
+							},
+							3
+						);
+					})
+					.catch(() => {
+						frappe.msgprint(__("Failed to copy URL"));
+					});
 			});
 		}
 	},
