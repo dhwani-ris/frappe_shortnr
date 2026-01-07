1667dfcda26d807a2a3f656892add8e9c174626d
dhwani-ankit <124347605+dhwani-ankit@users.noreply.github.com>
2026-01-07 12:12:28 +0530

    ci: update GitHub workflows

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   // Disable default 100-char (or 72-char) header length limit for commit messages
   rules: {
-    'header-max-length': [0, 'always', 100],
+    "header-max-length": [0, "always", 100],
   },
 };
